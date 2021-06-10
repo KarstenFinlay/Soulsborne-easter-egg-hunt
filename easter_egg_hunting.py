0561a7986848ec656672f057e168ce28021134bd
Karsten Finlay <KarstenFinlay@hotmail.com>
2021-06-10 11:18:42 +0100

    Refactored filtering files

--- easter_egg_hunting.py
+++ easter_egg_hunting.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import sys
 
+from os import listdir
+
+def question_yes_no(question):
+    answer = input(question).capitalize()
+    while True:
+        if answer != "Y" and answer != "N":
+            answewr = input("Please input Y or N\n")
+            continue
+
+        return answer == "Y"
+
 game_choices = ["DemonSouls", "DarkSouls", "DarkSouls2", "Bloodborne", "DarkSouls3", "Sekiro"]
 
 game_choice = input("Please select which game you wish to have an easter egg hunt in:\n1. Demon Souls\n2. Dark Souls\n3. Dark Souls 2\n4. Bloodborne\n5. Dark Souls 3\n6. Sekiro\n")
@@ -20,3 +31,26 @@ while True:
     except IndexError:
         game_choice = input("Please select one of the valid options:\n")
 
+game_files = listdir(f"./data/{game}")
+
+all_items_answer = question_yes_no("Would you like every item type to be included? Y/N\n")
+
+for file in game_files:
+    if not all_items_answer and not question_yes_no(f"Would you like {file} to be included? Y/N\n"):
+        continue
+
+    print(file)
+
+### CHALLENGES
+
+# 1. Getting all file names for game [X]
+
+# 2. Filtering files [X]
+
+# 3. Read each file indivdually
+
+# 4. Filter other colums
+
+# 5. Sample selected amount for each file
+
+# 6. Print out user's easter egg hunt list and write to file
